2ef0f29f49bd11d61a6e14a284b469f7fd76a784
Leanna <lmclarke@users.noreply.github.com>
2019-03-29 16:03:23 -0700

    Doubled up room class fixed

--- mygame/typeclasses/rooms.py
+++ mygame/typeclasses/rooms.py
@@ -13,16 +13,7 @@ from evennia.utils import list_to_string
 ROOM_NAME_COL = "|C"
 INDENT = "   "
 
-class Room(ContribRPRoom):
-    """
-    Rooms are like any Object, except their location is None
-    (which is default). They also use basetype_setup() to
-    add locks so they cannot be puppeted or picked up.
-    (to change that, use at_object_creation instead)
 
-    See examples/object.py for a list of
-    properties and methods available on all Objects.
-    """
 class Room(ContribRPRoom):
     """
     Rooms are like any Object, except their location is None
